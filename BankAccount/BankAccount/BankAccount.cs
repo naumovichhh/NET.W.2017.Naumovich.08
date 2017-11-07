1a87feb29ee490398ff925ad0c8e69cf52917be3
naumovichhh <32872842+naumovichhh@users.noreply.github.com>
2017-11-07 16:57:49 +0200

    Add files via upload

---
+++ BankAccount/BankAccount/BankAccount.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8
+{
+    public class BankAccount
+    {
+        public BankAccount(ulong newId, Person newOwner, 
+            IAccountType accType, double newBalance = 0)
+        {
+            id = newId;
+            owner = newOwner;
+            balance = newBalance;
+            accountType = accType;
+            bonuses = 0;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (balance - amount < 0)
+                throw new Exception("Not enough money");
+            balance -= amount;
+            accountType.ProcessBonuses(this, amount);
+        }
+
+        public void Put(double amount)
+        {
+            balance += amount;
+            accountType.ProcessBonuses(this, amount);
+        }
+
+        public void ProcessBonuses(double amount)
+        {
+            bonuses += amount;
+        }
+
+        public IAccountType type;
+        public ulong Id { get { return id; } }
+        public Person Owner { get { return owner; } }
+        public double Balance { get { return balance; } }
+        public double Bonuses { get { return bonuses; } }
+        public string AccountType { get { return accountType.ToString(); } }
+
+        IAccountType accountType;
+        private ulong id;
+        private Person owner;
+        private double balance;
+        private double bonuses;
+    }
+
+    public class Person
+    {
+        public Person(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+        public string Surname
+        {
+            get { return surname; }
+        }
+
+        private string name;
+        private string surname;
+    }
+}
---
+++ BankAccount/BankAccount/BankAccountsService.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace Day8
+{
+    class BankAccountsService
+    {
+        public void CreateAccount(BankAccount account)
+        {
+            using (var writer = new BinaryWriter(
+                File.Open(account.Id.ToString(), FileMode.Create)))
+            {
+                writer.Write(account.Id);
+                writer.Write(account.Owner.Name);
+                writer.Write(account.Owner.Surname);
+                writer.Write(account.Balance);
+                writer.Write(account.Bonuses);
+                writer.Write(account.AccountType);
+            }
+        }
+
+        public void CloseAccount(ulong id)
+        {
+            try
+            {
+                File.Delete(id.ToString());
+            }
+            catch
+            {
+                throw new Exception("Account doesn't exist");
+            }
+        }
+
+        public void Withdraw(ulong id, int amount)
+        {
+            BankAccount account;
+            try
+            {
+                using (var reader = new BinaryReader(File.OpenRead(id.ToString())))
+                {
+                    ulong s_id = reader.ReadUInt64();
+                    string name = reader.ReadString();
+                    string surname = reader.ReadString();
+                    double balance = reader.ReadDouble();
+                    double bonuses = reader.ReadDouble();
+                    string accountType = reader.ReadString();
+                    Assembly asm = Assembly.GetCallingAssembly();
+                    Type type = asm.GetType(accountType);
+                    object accTypeInstance = Activator.CreateInstance(type);
+                    account = new BankAccount(s_id, new Person(name, surname),
+                        (IAccountType)accTypeInstance, balance);
+                }
+                account.Withdraw(amount);
+                using (var writer = new BinaryWriter(
+                    File.Open(id.ToString(), FileMode.Create)))
+                {
+                    writer.Write(account.Id);
+                    writer.Write(account.Owner.Name);
+                    writer.Write(account.Owner.Surname);
+                    writer.Write(account.Balance);
+                    writer.Write(account.Bonuses);
+                    writer.Write(account.AccountType);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                throw new Exception("Account doesn't exist");
+            }
+        }
+
+        public void Put(ulong id, int amount)
+        {
+            BankAccount account;
+            try
+            {
+                using (var reader = new BinaryReader(File.OpenRead(id.ToString())))
+                {
+                    ulong s_id = reader.ReadUInt64();
+                    string name = reader.ReadString();
+                    string surname = reader.ReadString();
+                    double balance = reader.ReadDouble();
+                    double bonuses = reader.ReadDouble();
+                    string accountType = reader.ReadString();
+                    Assembly asm = Assembly.GetCallingAssembly();
+                    Type type = asm.GetType(accountType);
+                    object accTypeInstance = Activator.CreateInstance(type);
+                    account = new BankAccount(s_id, new Person(name, surname),
+                        (IAccountType)accTypeInstance, balance);
+                }
+                account.Put(amount);
+                using (var writer = new BinaryWriter(
+                    File.Open(id.ToString(), FileMode.Create)))
+                {
+                    writer.Write(account.Id);
+                    writer.Write(account.Owner.Name);
+                    writer.Write(account.Owner.Surname);
+                    writer.Write(account.Balance);
+                    writer.Write(account.Bonuses);
+                    writer.Write(account.AccountType);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                throw new Exception("Account doesn't exist");
+            }
+        }
+        
+    }
+}
